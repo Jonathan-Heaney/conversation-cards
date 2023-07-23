c490e1b01d1f9079a6d2ebe5584890a661dd42eb
Jonathan Heaney <jonathan.heaney@gmail.com>
2023-07-22 20:28:17 -0500

    Updated server code to add ability to update last_shown

--- conversation-cards-server/server.js
+++ conversation-cards-server/server.js
@@ -1,5 +1,3 @@
-// server.js
-
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -14,25 +12,41 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/card', async (req, res) => {
-  const result = await pool.query(
-    'SELECT * FROM holstee ORDER BY RANDOM() LIMIT 1'
+  // Get the ID of a card to be shown
+  const cardToBeShown = await pool.query(
+    "SELECT id FROM holstee WHERE (last_shown IS NULL OR last_shown < NOW() - INTERVAL '1 month') AND (last_answered IS NULL OR last_answered < NOW() - INTERVAL '1 year') ORDER BY RANDOM() LIMIT 1"
   );
-  res.json(result.rows[0]);
+  const cardId = cardToBeShown.rows[0]?.id;
+
+  if (cardId) {
+    // Update last_shown for this card
+    await pool.query('UPDATE holstee SET last_shown = NOW() WHERE id = $1', [
+      cardId,
+    ]);
+
+    // Fetch and return the updated card
+    const result = await pool.query('SELECT * FROM holstee WHERE id = $1', [
+      cardId,
+    ]);
+    res.json(result.rows[0]);
+  } else {
+    res.status(404).json({ message: 'No card available' });
+  }
 });
 
-app.put('/card/:id/snooze', async (req, res) => {
-  const { id } = req.params;
-  const result = await pool.query(
-    "UPDATE holstee SET last_shown = NOW() + INTERVAL '1 month' WHERE id = $1",
-    [id]
-  );
-  res.json(result.rowCount > 0);
-});
+// app.put('/card/:id/next', async (req, res) => {
+//   const { id } = req.params;
+//   const result = await pool.query(
+//     'UPDATE holstee SET last_shown = NOW() WHERE id = $1',
+//     [id]
+//   );
+//   res.json(result.rowCount > 0);
+// });
 
 app.put('/card/:id/answer', async (req, res) => {
   const { id } = req.params;
   const result = await pool.query(
-    "UPDATE holstee SET last_answered = NOW() + INTERVAL '1 year' WHERE id = $1",
+    'UPDATE holstee SET last_answered = NOW() WHERE id = $1',
     [id]
   );
   res.json(result.rowCount > 0);
